8112f4ed99dfe2d40c0e54cb35f61a0dfb14429e
Martav69 <martav69200@gmail.com>
2024-08-13 14:40:48 +0200

    Create Entity
    Creation of Category entity
    Populate the category entity with an array
    
    Creation of the Event entity with relation to Category
    
    ** next step **
    Start populate the Event Entity with Faker to generate data
    Start making the basic views.
    
    Navbar / Landing Page
    
    Views Category -> list of it and related Event on click
    Views Event -> list of card event
    detail on click

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
+
 
 class AppFixtures extends Fixture
 {
+
+    private const CATEGORIES_NAMES = ['Football', 'Basket', 'Tennis','Rugby' ,'MMA','Arts Martiaux','Athlétisme','Natation','Course', 'Volleyball'];
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $faker = Faker\Factory::create('fr_FR');
+       
+
+        $categories = [];
+
+        for ($i=0; $i < count(self::CATEGORIES_NAMES) ; $i++) { 
+            $category = new Category();
+            $category->setName(self::CATEGORIES_NAMES[$i]);
+
+            $manager->persist($category);
+            $categories[$i] = $category;
+        }
 
         $manager->flush();
     }
---
+++ src/Entity/Event.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picFileName = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $startDate = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $endDate = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?int $totalSeats = null;
+
+    #[ORM\Column]
+    private ?int $availableSeats = null;
+
+    #[ORM\ManyToOne(inversedBy: 'events')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPicFileName(): ?string
+    {
+        return $this->picFileName;
+    }
+
+    public function setPicFileName(?string $picFileName): static
+    {
+        $this->picFileName = $picFileName;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): static
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): static
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getTotalSeats(): ?int
+    {
+        return $this->totalSeats;
+    }
+
+    public function setTotalSeats(int $totalSeats): static
+    {
+        $this->totalSeats = $totalSeats;
+
+        return $this;
+    }
+
+    public function getAvailableSeats(): ?int
+    {
+        return $this->availableSeats;
+    }
+
+    public function setAvailableSeats(int $availableSeats): static
+    {
+        $this->availableSeats = $availableSeats;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
