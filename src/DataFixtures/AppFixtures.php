8dbf1dcf2cf66927af641773a3e73d8f559a0c6c
Martav69 <martav69200@gmail.com>
2024-08-13 19:25:24 +0200

    Correct Entity
    Correction de bug via les migrations suite à tentative de populate de la table country

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,9 +3,12 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\Country;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
+
+
 
 
 class AppFixtures extends Fixture
@@ -15,10 +18,12 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-
+        // load faker pour l'utiliser en FR
         $faker = Faker\Factory::create('fr_FR');
        
 
+
+        // Populate les category
         $categories = [];
 
         for ($i=0; $i < count(self::CATEGORIES_NAMES) ; $i++) { 
@@ -28,6 +33,19 @@ class AppFixtures extends Fixture
             $manager->persist($category);
             $categories[$i] = $category;
         }
+
+        // Populate des pays en utilisant umpirsky package
+       // Chemin vers le fichier JSON 
+       $filePath = __DIR__ . '/../../vendor/umpirsky/country-list/data/fr/country.json';
+       $countries = json_decode(file_get_contents($filePath), true);
+
+       foreach ($countries as $countryCode => $countryName) {
+           $country = new Country();
+           $country->setName($countryName);
+           $manager->persist($country);
+       }
+
+
 
         $manager->flush();
     }
