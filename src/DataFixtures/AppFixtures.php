9de0028fe565164e5e2b1f415f36be9fa926d019
Martav69 <martav69200@gmail.com>
2024-08-14 11:12:02 +0200

    Fake Data generated
    All the entities are now populated
    
    Next step :
    start making the controller to add the views
    make:crud for entities
    navbar with flowbite

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,7 +3,9 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\City;
 use App\Entity\Country;
+use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
@@ -38,11 +40,53 @@ class AppFixtures extends Fixture
        // Chemin vers le fichier JSON 
        $filePath = __DIR__ . '/../../vendor/umpirsky/country-list/data/fr/country.json';
        $countries = json_decode(file_get_contents($filePath), true);
+        $pays = [];
 
        foreach ($countries as $countryCode => $countryName) {
            $country = new Country();
            $country->setName($countryName);
            $manager->persist($country);
+
+           $pays[] = $country;
+       }
+
+       // Populate des villes via faker 
+       $villes = [];
+       for ($i=0; $i < 300 ; $i++) { 
+        $ville = new City();
+        $ville->setName($faker->city);
+        $ville->setCountry($faker->randomElement($pays));
+        $manager->persist($ville);
+
+        $villes[$i] = $ville;
+       }
+
+       // Populate des events
+       for ($i=0; $i < 10 ; $i++) { 
+
+         $event = new Event();
+         $event->setTitle($faker->catchPhrase);
+         $event->setDescription($faker->paragraph(3,true));
+         $event->setPicFileName($faker->imageUrl(640,480,'sports',true,'event'));
+
+         // gestion des date avec clone modifié pour avoir une date de fin supérieur a la date de début
+         $startDate = $faker->dateTimeBetween('-1 month', '+1 month');
+         $event->setStartDate($startDate);
+         $endDate = (clone $startDate)->modify('+1 month');
+         $event->setEndDate($faker->dateTimeBetween($startDate, $endDate));
+
+         $event->setPrice($faker->randomFloat(2,20,200));
+
+         $totalSeats = $faker->numberBetween(10,300);
+         $event->setTotalSeats($totalSeats);
+
+         $event->setAvailableSeats($faker->numberBetween(0, $totalSeats));
+
+         $event->setCategory($faker->randomElement($categories));
+         $event->addCity($faker->randomElement($villes));
+
+         $manager->persist($event);
+
        }
 
 
